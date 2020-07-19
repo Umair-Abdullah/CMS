a89f5a623bd8cb2c7b97822b83812b3cf6752b38
Wajahat Ahmed <wajahatahmed1606@gmail.com>
2020-07-19 21:50:46 +0500

    Vgg16 model and prediction

--- scripts/face_detection_demo.py
+++ scripts/face_detection_demo.py
@@ -36,12 +36,29 @@ def done_clicked():
 B = Button(gui, text="Done", command=done_clicked)
 B.grid(column=5, row=2)
 gui.mainloop()
-label="images/"+label
+
+label1=os.path.join(os.getcwd(), "images", "train")
+label2=os.path.join(os.getcwd(), "images", "test")
+
+#train test directory
+try:
+    os.mkdir(label1)
+    os.mkdir(label2)
+except:
+    print("")
+
+label_test=os.path.join(label2, label)
+label=os.path.join(label1, label)
+
+
 # creating folder for data of person
 try:
     os.mkdir(label)
+    os.mkdir(label_test)
 except:
-    print("Error")
+    print(" ")
+
+print(label,"   ",label_test)
 #End of name dialog
 
 
@@ -59,32 +76,40 @@ def test(val):
 cap = cv2.VideoCapture(0)
 # GUI
 save = 0
+pic=""
 while True:
     # reading frame from captured video
     ret, frame = cap.read()
 
     # convert to grayscale
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    #gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
     # Detection of Faces
-    faces = face_cascade.detectMultiScale(gray, 1.1, 4)
+    faces = face_cascade.detectMultiScale(frame, 1.1, 4)
 
     # draw box on faces
     for (x, y, w, h) in faces:
         crop_face = frame[y:y + h, x:x + w]
+        #crop_face = cv2.blur(crop_face,(2,2))
         crop_face=cv2.resize(crop_face, (256,256), interpolation=cv2.INTER_AREA)
-        crop_face=cv2.cvtColor(crop_face,cv2.COLOR_BGR2GRAY)
         save = save + 1
-        if save < 100:
-            print(label + "/face" + str(save) + ".jpg")
-            cv2.imwrite(label + "/face" + str(save) + ".jpg", crop_face)
+        if save <= 200:
+            pic=os.path.join(label, "face"+str(save)+".jpg")
+            print(pic)
+            cv2.imwrite(pic, crop_face)
+        if save>200 and save<=300:
+            pic = os.path.join(label_test, "face" + str(save) + ".jpg")
+            print(pic)
+            cv2.imwrite(pic, crop_face)
         cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 155, 55), 2)
 
 
     # Show inside a window
     cv2.imshow('Camera', frame)
-    cv2.createTrackbar("Pictures", "Camera", r, 255, test)
+    #cv2.createTrackbar("Pictures", "Camera", r, 255, test)
 
 
     # End when Enter is Pressed
     if cv2.waitKey(1) == 13: break
+cap.release()
 cv2.destroyAllWindows()
